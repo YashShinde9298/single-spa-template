d456e73138a8a6a394b055874953d5ea58db5496
Yash Shinde <yashshinde337@gmail.com>
2024-06-20 14:57:05 +0530

    fixes page refreshing

--- react-navbar/src/root.component.js
+++ react-navbar/src/root.component.js
@@ -1,10 +1,15 @@
 import './style.css';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 export default function Root(props) {
   return (<>
-    <nav>
-      <a href="/">Home</a>
-      <a href="/blogs">Blog</a>
-    </nav>
+    <Router>
+      <nav>
+        {/* <a href="/">Home</a>
+      <a href="/blogs">Blog</a> */}
+        <Link to='/'>Home</Link>
+        <Link to='/blogs'>Blog</Link>
+      </nav>
+    </Router>
   </>)
 }
