98865ae9f9d1ee688f8be6cbc51126e9e7eb8de3
Yash Shinde <yashshinde337@gmail.com>
2024-06-20 14:26:38 +0530

    init singls-spa-template

---
+++ react-blog-app/src/root.component.js
@@ -0,0 +1,5 @@
+export default function Root(props) {
+  return (<>
+    <h1>Welcome to Blogs Section</h1>
+  </>);
+}
---
+++ react-navbar/src/root.component.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function Root(props) {
+  return (<>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/blogs">Blog</a>
+    </nav>
+  </>)
+}
